e3860b207d9d11058f35aaf650186d21fbdece60
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2019-12-14 23:02:49 -0500

    html routes set, server setup began

---
+++ Develop/htmlRoutes.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = function (app) {
+
+    app.get("/index", function (req, res) {
+        res.sendFile(path.join(__dirname, "../develop/public/index.html"));
+    });
+
+    app.get("/notes", function (req, res) {
+        res.sendFile
+            (path.join(__dirname, "../develop/public/notes.html"));
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile
+            (path.join(__dirname, "../develop/public/index.html"));
+    });
+
+};
---
+++ Develop/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
+
+app.listen(PORT, () => {
+    console.log(`server on port ${PORT}`)
+});
